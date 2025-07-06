bc9721f2c65d334a7188fd4b3f42df7bdaeafd0e
Rikhin <rikhinkavuru@icloud.com>
2025-07-05 20:10:27 -0400

    Fix: header overlay is pointer-events-none so all header buttons/links work with modal open

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -18,14 +18,19 @@ const DialogOverlay = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Overlay>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Overlay>
 >(({ className, ...props }, ref) => (
-  <DialogPrimitive.Overlay
-    ref={ref}
-    className={cn(
-      "fixed left-0 right-0 top-20 bottom-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
-      className
-    )}
-    {...props}
-  />
+  <>
+    {/* Transparent, non-interactive overlay for header */}
+    <div className="fixed left-0 right-0 top-0 h-20 z-50 pointer-events-none" />
+    {/* Normal overlay for rest of page */}
+    <DialogPrimitive.Overlay
+      ref={ref}
+      className={cn(
+        "fixed left-0 right-0 top-20 bottom-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+        className
+      )}
+      {...props}
+    />
+  </>
 ))
 DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
 
